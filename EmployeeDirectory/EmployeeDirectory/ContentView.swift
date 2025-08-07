a5f9aa4b6bebfbb73ac42385598790e891c0e4be
Jaison-Abraham <jaisonabraham8606@gmail.com>
2025-08-07 14:15:31 -0400

    Initial Commit

---
+++ EmployeeDirectory/EmployeeDirectory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmployeeDirectory
+//
+//  Created by JAISON ABRAHAM on 2025-08-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
